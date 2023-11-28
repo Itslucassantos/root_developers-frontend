5e9405c9a165f74ea76a6adbc845a3d6fc33b33b
Lucas Santos <ls6865316@gmail.com>
2023-11-28 15:22:09 -0300

    Feito alguns ajustes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    API_URL: 'http://localhost:8081/api',   
+    API_URL: 'https://calculador-api.onrender.com/api',   
     CLIENT_ID: 'clientFrontend',
     SECRET_ID: 'clientFrontend',
   };
